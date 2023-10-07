39123833f50b4ddf11df6d2db64eeff0dfb81495
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-08 00:53:42 +0300

    completed lab 4 task1, problem 2

--- labs/Lab4/task1.rb
+++ labs/Lab4/task1.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+# problem 1
 def find_repeat_groups(arr)
   i = 0
   count = 0
@@ -15,11 +16,31 @@ def find_repeat_groups(arr)
   count
 end
 
+#problem 2
+def calculate_even_sum_index(arr)
+  element_sum = 0
+  index_sum = 0
+  arr.each_with_index do |element, index|
+    el = element.to_i
+    next if el.odd?
+    element_sum += el
+    index_sum += index
+  end
+  [element_sum, index_sum]
+end
+
+
 class TestCalculateFunction < MiniTest::Test
   def test_find_repeat_groups
     assert_equal 0, find_repeat_groups([1, 2, 3, 4, 1, 2])
     assert_equal 1, find_repeat_groups([1, 2, 2, 3])
     assert_equal 1, find_repeat_groups([1, 2, 2, 2, 2, 2, 3])
     assert_equal 3, find_repeat_groups([1, 1, 2, 2, 3, 2, 2])
+    assert_equal 0, find_repeat_groups([])
+  end
+
+  def test_calculate_even_sum_index
+    assert_equal [6,4], calculate_even_sum_index([1.2, 2.2, 3.8, 4.5])
+    assert_equal [0,0], calculate_even_sum_index([])
   end
 end
