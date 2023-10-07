0a9a296f1b6c7166942127c471c15b20b335530b
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-07 11:16:06 +0300

    added more test cases

--- labs/Lab1/test.rb
+++ labs/Lab1/test.rb
@@ -9,6 +9,8 @@ class TestCalculateFunction < MiniTest::Test
   def test_calculate
     assert comp_float(calculate(1,1,1), 1144.1310305)
     assert comp_float(calculate(1,1,2), 423.02359776)
+    assert comp_float(calculate(3,0,0), 1.57079632679)
+    assert comp_float(calculate(1,0.02,2), -7157.58900444)
     assert_raises(ZeroDivisionError) do
       calculate(0, 0, 0)
     end
