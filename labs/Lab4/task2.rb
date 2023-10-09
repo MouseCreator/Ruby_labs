e81b02ea0548ab5bf6a57ede24c76aabaab7d885
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-09 13:34:22 +0300

    added outer product

--- labs/Lab4/task2.rb
+++ labs/Lab4/task2.rb
@@ -24,7 +24,7 @@ def vector_scalar_product(v1,v2)
   v1.inner_product(v2)
 end
 
-def vector_outer_product(v1,v2)
+def vector_v_product(v1, v2)
   v1.cross(v2)
 end
 
@@ -38,6 +38,18 @@ end
 
 def matrix_by_vector(a, v)
   a * v
+end
+
+def outer_product(v1, v2)
+  n = v1.length
+  m = v2.length
+  result = Array.new(v1.length) { Array.new(v2.length, 0) }
+  (0...n).each do |i|
+    (0...m).each do |j|
+      result[i][j] = v1[i] * v2[j]
+    end
+  end
+  result
 end
 
 
@@ -109,7 +121,7 @@ class TestMatrixOperations < MiniTest::Test
   def test_vector_product
     v1 = Vector[2, 4, 6]
     v2 = Vector[1, 5, 9]
-    assert_equal(Vector[6, -12, 6], vector_outer_product(v1,v2))
+    assert_equal(Vector[6, -12, 6], vector_v_product(v1, v2))
   end
 
   def test_vector_norm
@@ -140,5 +152,11 @@ class TestMatrixOperations < MiniTest::Test
       array_matrix_vector_mult(matrix,vector)
     end
     assert_equal([50,122],  array_matrix_vector_mult(matrix,vector2))
+  end
+
+  def test_outer_product
+    vector = [1,2,3]
+    vector2 = [4,5,6]
+    assert_equal([[4,5,6],[8,10,12],[12,15,18]], outer_product(vector,vector2))
   end
 end
